2dd3653d7224137b06cc344fa6028bf47767602e
BW <beweeks71@hotmail.com>
2022-06-26 21:46:03 -0400

    popup

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-$("i").click(function () {
-  $("ul").toggleClass("open");
+window.addEventListener("load", function(){
+    setTimeout(
+        function open(event){
+            document.querySelector(".popup").style.display = "block";
+        },
+        2000
+    )
+});
+document.querySelector("#close").addEventListener("click", function(){
+    document.querySelector(".popup").style.display = "none";
 });
