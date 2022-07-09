33778249f050ece87ef28163d583e069a91f6f8a
BW <beweeks71@hotmail.com>
2022-07-08 21:31:01 -0400

    jquery

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 window.addEventListener("load", function(){
-    setTimeout(
-        function open(event){
-            document.querySelector(".popup").style.display = "block";
-        },
-        2000
-    )
+  setTimeout(
+    function open(event){
+      $(document).ready(function(){
+      $(".popup").slideToggle(4000);
+  },
+
+)
+});
 });
 document.querySelector("#close").addEventListener("click", function(){
-    document.querySelector(".popup").style.display = "none";
+document.querySelector(".popup").style.display = "none";
 });
